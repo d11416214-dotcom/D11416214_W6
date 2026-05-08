47b802b29bdb8b0b6d9cd209f6218381c8ee48e2
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-05-08 11:14:29 +0800

    Add files via upload

---
+++ D11416214_W6/D11416214_W6/D11416214_W6/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Person
+{
+	public virtual string GetName()
+	{
+		return "Person";
+	}
+}
+public class Student : Person
+{
+	public override string GetName()
+	{
+		return "Student";
+	}
+}
+public class  People
+{
+    public string GetName()
+    {
+        return "People";
+    }
+}
+public class Teacher : People
+{
+	public new string GetName()
+	{
+		return "Teacher";
+	}
+}
+
+
---
+++ D11416214_W6/D11416214_W6/D11416214_W6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace D11416214_W6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person s = new Student();
+            Console.WriteLine(s.GetName());
+            Person s2= new Student();
+            Console.WriteLine(s2.GetName());
+            Console.WriteLine(((Person)s2).GetName());
+            Console.WriteLine("***********");
+            Teacher t= new Teacher();
+            Console.WriteLine(t.GetName());
+            People t2=new Teacher();
+            Console.WriteLine(t2.GetName());
+            Console.WriteLine(((Teacher)t2).GetName());
+        }
+    }
+}
